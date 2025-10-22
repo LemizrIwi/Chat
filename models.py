8823e1dcf4d1fe38b41cae7473983115b79e38fb
LemizrIwi <89360645+LemizrIwi@users.noreply.github.com>
2025-10-22 20:15:36 +0200

    Update models.py

--- models.py
+++ models.py
@@ -1,10 +1,18 @@
-# models.py
-from sqlalchemy import Column, Integer, String, Text, DateTime, func
+from sqlalchemy import Column, Integer, String, DateTime, func
 from db import Base
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, index=True)
+    password_hash = Column(String)
+    online = Column(Integer, default=0)  # 0=offline, 1=online
+    color = Column(String, default="#FFFFFF")
+    is_admin = Column(Integer, default=0)  # 1 = Admin
 
 class Message(Base):
     __tablename__ = "messages"
     id = Column(Integer, primary_key=True, index=True)
-    username = Column(String(100), nullable=False)
-    content = Column(Text, nullable=False)
+    username = Column(String)
+    content = Column(String)
     timestamp = Column(DateTime(timezone=True), server_default=func.now())
